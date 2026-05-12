fce299d2499aec8ce93822627fb60a918cbe30b0
Raynald-Gab <raynaldgdgbarrion@gmail.com>
2026-05-13 00:18:58 +0800

    first commit

---
+++ src/Practice1/Variables.java
@@ -0,0 +1,18 @@
+package Practice1;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "David sdpt";
+        System.out.println("My name is " + name);
+
+        int num1 = 24;
+        System.out.println("I'am " + num1 + " Years Old");
+
+        double num2 = 2.25;
+        System.out.println("My GPA is " + num2);
+
+        char num = 'O';
+        System.out.println("My blood type is " + num);
+    }
+
+}
