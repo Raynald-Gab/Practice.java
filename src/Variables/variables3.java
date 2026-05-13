b880a0624f0eac833053d3168a38f4bbc695eaaa
Raynald-Gab <raynaldgdgbarrion@gmail.com>
2026-05-13 20:59:45 +0800

    thrid commit

---
+++ src/Variables/variables3.java
@@ -0,0 +1,31 @@
+package Variables;
+
+public class variables3 {
+    public static void main (String[] agrs){
+        double balance = 250000.75;
+        String game = "alive";
+        float file = 15.8f;
+        char grade = 'A';
+        double num1 = 2026.;
+        float rating = 4.9f;
+        int followers = 1250;
+        float temp = 89.6f;
+        char code = 'X';
+        String server = "offline";
+
+        System.out.println("The bank balanace is " + balance);
+        System.out.println("The game is " + game);
+        System.out.println("The file is " + file);
+        System.out.println("The grade is " + grade);
+        System.out.println("The rating is " + rating);
+        System.out.println("The followers is " + followers);
+        System.out.println("The temperature is " + temp);
+        System.out.println("The code is " + code);
+        System.out.println("The server is " + server);
+
+
+
+
+
+    }
+}
