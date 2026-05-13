19425445147f846245f658dd03d9693d6b561430
Raynald-Gab <raynaldgdgbarrion@gmail.com>
2026-05-13 17:54:46 +0800

    Second commit

--- src/Variables/Variables.java
+++ src/Variables/Variables.java
@@ -1,4 +1,4 @@
-package Practice1;
+package Variables;
 
 public class Variables {
     public static void main(String[] args) {
---
+++ src/Variables/variables2.java
@@ -0,0 +1,28 @@
+package Variables;
+
+public class variables2 {
+    public static void main (String[] agrs) {
+
+        int age = 18;
+        double cost = 15999.50;
+        String name1 = "Carla";
+        char section = 'B';
+        String school = "Lipa Aceademy";
+        double weight = 2.5;
+        int siblings = 3;
+        String account = "Active";
+        double num2 = 36.7;
+        char letter = 'z';
+
+        System.out.print("alex is " + age + " years old" );
+        System.out.println ("The phone cost " + cost + " pesos.");
+        System.out.println (name1 + " failed the interview" );
+        System.out.println("The section is " + section);
+        System.out.println("The school is " + school);
+        System.out.println("The laptop weights" + weight + " Kilograms.");
+        System.out.println("David has " + siblings + " siblings." );
+        System.out.println("The account is " + account);
+        System.out.println("The temperature is " + num2 + " degrees celcius");
+        System.out.println("The first letter of the word is " + letter);
+    }
+}
