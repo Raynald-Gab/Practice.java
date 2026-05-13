670e66e27f5247ba283db4c84758502d2fb59382
Raynald-Gab <raynaldgdgbarrion@gmail.com>
2026-05-13 23:45:59 +0800

    Third Commit

---
+++ src/Arithmetic/Arithmetic.java
@@ -0,0 +1,13 @@
+package Arithmetic;
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String [] args) {
+        String x;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first number: ");
+        x = scanner.next();
+        System.out.println("Enter your second number: " + x);
+    }
+}
